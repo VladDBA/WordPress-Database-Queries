64e79d2b45b73c45765026b30377d45ee8b4903e
Vlad Drumea <48413726+VladDBA@users.noreply.github.com>
2024-09-22 14:26:58 +0300

    added some files

---
+++ GetPostsByLikes.sql
@@ -0,0 +1,14 @@
+SELECT
+    p.post_name,
+    p.post_title,
+    p.post_date,
+    pm.meta_key,
+    pm.meta_value
+FROM
+    wpkx_postmeta pm
+INNER JOIN wpkx_posts p ON
+    pm.post_id = p.ID
+WHERE
+    pm.meta_key = '_post_like_count'
+ORDER BY
+    CAST(pm.meta_value AS INTEGER) DESC;
