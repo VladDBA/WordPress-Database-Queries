79509cf326b97bfc3eb4819e2d2db899b2c530d8
Vlad Drumea <48413726+VladDBA@users.noreply.github.com>
2024-09-28 14:53:35 +0300

    commented some columns, added URL and Mardkown URL

--- GetPostsByViews.sql
+++ GetPostsByViews.sql
@@ -1,9 +1,13 @@
 SELECT
-    p.post_name,
+    -- p.post_name,
     p.post_title,
     p.post_date,
-    pm.meta_key,
-    pm.meta_value
+    -- pm.meta_key,
+    pm.meta_value AS views,
+    -- p.guid,
+    /*might want to adjust based on how your post URLs are structured*/
+    CONCAT(SUBSTRING_INDEX(p.guid,'?',1),REPLACE(CAST(CAST(post_date as date) AS VARCHAR(20)),'-','/'), '/', post_name) AS URL,
+    CONCAT('- [', p.post_title, ']', '(', SUBSTRING_INDEX(p.guid,'?',1),REPLACE(CAST(CAST(post_date as date) AS VARCHAR(20)),'-','/'), '/', post_name, ')') AS MarkdownFormattedLink
 FROM
     wpkx_postmeta pm
 INNER JOIN wpkx_posts p ON
@@ -11,4 +15,5 @@ INNER JOIN wpkx_posts p ON
 WHERE
     pm.meta_key = 'penci_post_views_count'
 ORDER BY
-    CAST(pm.meta_value AS INTEGER) DESC; 
+    CAST(pm.meta_value AS INTEGER) DESC
+LIMIT 10;
