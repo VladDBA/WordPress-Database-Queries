f71ceb8bd8617aba02edee420a96a940f5b21693
Vlad Drumea <48413726+VladDBA@users.noreply.github.com>
2024-10-13 19:10:06 +0300

    added keyphrase info to SEO index scrore query

--- GetPostsWithWorstYaostScores.sql
+++ GetPostsWithWorstYaostScores.sql
@@ -6,12 +6,21 @@ SELECT
     p.post_title,
     p.post_date,
     pm.meta_value AS yoast_seo_index,
+    pmfocuskw.meta_value AS yoast_seo_keyphrase, 
+    pmkwsyn.meta_value AS yoast_seo_keyphrase_synonyms,
+    pmfkws.meta_value AS yoast_seo_related_keyphrase,
     /*might want to adjust based on how your post URLs are structured*/
     CONCAT(SUBSTRING_INDEX(p.guid,'?',1),REPLACE(CAST(CAST(p.post_date as date) AS VARCHAR(20)),'-','/'), '/', p.post_name) AS URL
 FROM
     wpkx_postmeta pm
 INNER JOIN wpkx_posts p ON
     pm.post_id = p.ID
+LEFT JOIN wpkx_postmeta pmfkws ON
+    pm.post_id = pmfkws.post_id AND pmfkws.meta_key = '_yoast_wpseo_focuskeywords'
+LEFT JOIN wpkx_postmeta pmfocuskw ON
+    pm.post_id = pmfocuskw.post_id AND pmfocuskw.meta_key = '_yoast_wpseo_focuskw'
+LEFT JOIN wpkx_postmeta pmkwsyn ON
+    pm.post_id = pmkwsyn.post_id AND pmkwsyn.meta_key = '_yoast_wpseo_keywordsynonyms'
 WHERE
     pm.meta_key = '_yoast_wpseo_linkdex'
     AND p.post_status = 'publish'
